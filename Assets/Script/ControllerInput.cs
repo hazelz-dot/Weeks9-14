d91f65c0558b8de6d6465a1fa421bb82fa388201
zhou42 <zhou42@sheridancollege.ca>
2026-03-11 11:24:36 -0400

    controller code

---
+++ Assets/Script/ControllerInput.cs
@@ -0,0 +1,45 @@
+using System.Net.Mime;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ControllerInput : MonoBehaviour
+{
+    public float speed = 5;
+    public Vector2 movement;
+    public AudioSource SFX; 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //use with a stick
+        transform.position += (Vector3)movement * speed * Time.deltaTime; 
+
+        //use with mouse position
+        //transform.position.movement; 
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movement = context.ReadValue <Vector2>(); 
+    }
+
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        Debug.Log("Attack!" + context.phase); 
+        if(context.performed == true)
+        {
+            SFX.Play(); 
+        }
+    }
+
+    public void OnPoint(InputAction.CallbackContext context)
+    {
+        //the same as Mouse.current.position.ReadValue()
+        movement = Camera.main.ScreenToWorldPoint(context.ReadValue<Vector2>()); 
+    }
+}
