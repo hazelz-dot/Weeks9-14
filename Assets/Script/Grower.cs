05a1685beb5e1a731e2441f990c9f3146ac1a729
zhou42 <zhou42@OA-S141-21>
2026-03-16 09:48:28 -0400

    make a coroutine wait or another coroutine

---
+++ Assets/Script/Grower.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using UnityEngine;
+
+public class Grower : MonoBehaviour
+{
+
+    public Transform tree;
+    public Transform apple;
+
+    Coroutine growingCoroutine;
+    Coroutine treeCoroutine;
+    Coroutine appleCoroutine; 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tree.localScale = Vector2.zero; 
+        apple.localScale = Vector2.zero;
+    }
+
+    public void StartTreeGrowing()
+    {
+        if (growingCoroutine != null)
+        {
+            StopCoroutine(growingCoroutine);
+        }
+
+        if (treeCoroutine != null)
+        {
+            StopCoroutine(treeCoroutine); 
+        }
+
+        if(appleCoroutine != null)
+        {
+            StopCoroutine(appleCoroutine);
+        }
+        
+        growingCoroutine = StartCoroutine(Growing()); 
+    }
+
+    IEnumerator Growing()
+    {
+        yield return treeCoroutine = StartCoroutine(GrowTree()); 
+        yield return appleCoroutine = StartCoroutine(GrowApple());
+    }
+
+    IEnumerator GrowTree()
+    {
+        Debug.Log("Starting to grow the tree");
+        float t = 0;
+        tree.localScale = Vector2.zero; 
+        apple.localScale = Vector2.zero;
+
+        while (t < 1)
+        {
+            t += Time.deltaTime; 
+            tree.localScale = Vector2.one*t;
+            yield return null; 
+        }
+    }
+
+    IEnumerator GrowApple()
+    {
+
+    }
+}
